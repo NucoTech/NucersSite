ada044f784f314b0f098d9fd6df5afcf46fee652
HerbertHe <Herbert.He0229@gmail.com>
2020-07-05 18:35:40 +0800

    fix: props and states

--- site/components/common/MarkdownRenderer.tsx
+++ site/components/common/MarkdownRenderer.tsx
@@ -5,26 +5,18 @@ interface IMdRendererProps {
     content: string
 }
 
-interface IMdRendererStates {
-    content: string
-}
-
 /**
  * Markdown文件渲染组件
  */
 export default class MarkdownRenderer extends React.Component<
-    IMdRendererProps,
-    IMdRendererStates
+    IMdRendererProps
 > {
     constructor(props: IMdRendererProps) {
         super(props)
-        this.state = {
-            content: this.props.content || "",
-        }
     }
     private nucersMdRenderer = React.createRef<HTMLDivElement>()
     componentDidMount() {
-        const { content } = this.state
+        const { content } = this.props
         Vditor.preview(this.nucersMdRenderer.current, content)
     }
     render() {
