99a3a726e403b382f88f3b9e528f77b616b6f8b6
HerbertHe <Herbert.He0229@gmail.com>
2020-07-10 00:51:20 +0800

    feat: search engine

--- site/components/common/NavBar.tsx
+++ site/components/common/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "@styles/index.css"
 import IconFont from "./IconFont"
+
 const navBarStyle = require("@styles/components/common/NavBar.css")
 
 export interface INav {
@@ -13,15 +14,65 @@ interface INavProps {
     navs?: Array<INav>
 }
 
+interface INavStates {
+    search: string
+}
+
+interface SearchType {
+    type: string
+    content: string | Array<string>
+}
+
 /**
  * 导航栏组件
  */
-export default class NavBar extends React.Component<INavProps> {
+export default class NavBar extends React.Component<INavProps, INavStates> {
     constructor(props: INavProps) {
         super(props)
+        this.state = {
+            search: "",
+        }
     }
+
+    changeSearch = (e: any) => {
+        this.setState({
+            search: e.target.value || "",
+        })
+    }
+
+    search4Result = () => {
+        const { search } = this.state
+        // 判断search是否为空
+        if (!search) return
+        // 正则表达式语法解析
+        const searchArray: Array<string> = search.split("&&")
+        const searchRegExp: RegExp = /[\s]+([\S]+):[\s]+([\s\S]+)/
+        // 合法搜索头
+        const validHead: Array<string> = ["title", "groups", "tags"]
+        // 搜索内容节点
+        let searchNodes: Map<string, string | Array<string>> = new Map<
+            string,
+            string | Array<string>
+        >()
+        searchArray.forEach((item: string) => {
+            const ans: Array<string> = item.match(searchRegExp)
+            // 为null匹配的时候  不合法搜索头的时候 出现title搜索头的时候
+            if (!ans || !validHead.includes(ans[1]) || ans[1] === "title") {
+                searchNodes.set("title", item.trim())
+            } else {
+                searchNodes.set(
+                    ans[1],
+                    ans[2].split("|").map((item: string) => item.trim())
+                )
+            }
+        })
+        console.log(searchNodes)
+        // 跳转搜索答案页
+    }
+
     render() {
         const { navs } = this.props
+        const { search } = this.state
         return (
             <div className={navBarStyle.navbar}>
                 <div className={navBarStyle.navbarLeft}>
@@ -46,11 +97,26 @@ export default class NavBar extends React.Component<INavProps> {
                     )}
                 </div>
                 <div className={navBarStyle.navbarRight}>
-                    <input
-                        type="search"
-                        placeholder="search"
-                        className={navBarStyle.navbarSearch}
-                    />
+                    <div className={navBarStyle.navbarSearchBox}>
+                        <input
+                            type="search"
+                            placeholder="search"
+                            className={navBarStyle.navbarSearch}
+                            onChange={(e) => this.changeSearch(e)}
+                            autoComplete="off"
+                        />
+                        <ul className={navBarStyle.navbarSearchTips}>
+                            <li onClick={() => this.search4Result()}>
+                                搜索{search ? `"${search}"` : ""}
+                            </li>
+                            <li>
+                                <a href="/posts/syntax">
+                                    了解Nucers搜索语法...
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
                     <div className={navBarStyle.navbarAddIcon}>
                         <IconFont
                             type="nucers-add"
