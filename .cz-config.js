93d062364b00ec00061da0f4e140a739c3e72055
HerbertHe <Herbert.He0229@gmail.com>
2020-08-16 20:27:25 +0800

    :wrench: chore: update cz config
    更改cz的自定义提交问题流

--- .cz-config.js
+++ .cz-config.js
@@ -49,7 +49,7 @@ module.exports = {
 
     allowCustomScopes: true,
     allowBreakingChanges: ["feat", "fix"],
-    skipQuestions: ["body"],
+    skipQuestions: ["scope"],
 
     // limit subject length
     subjectLimit: 100,
